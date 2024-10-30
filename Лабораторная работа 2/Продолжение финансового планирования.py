a0bcacaa6c8d474b006db40c2866b6bdaa22f6bf
Ksuli <120677255+Ksuli@users.noreply.github.com>
2024-10-30 23:17:21 +0300

    Add files via upload

---
+++ Лабораторная работа 2/Продолжение финансового планирования.py
@@ -0,0 +1,16 @@
+salary = 5000  # Ежемесячная зарплата
+spend = 6000  # Траты за первый месяц
+months = 10  # Количество месяцев, которое планируется протянуть без долгов
+increase = 0.03  # Ежемесячный рост цен
+
+# Рассчитайте подушку безопасности
+money_capital = 0
+for month in range(months):
+    if month > 0:
+        spend *= 1 + increase
+    deficit = spend - salary
+    if deficit > 0:
+        money_capital += deficit
+
+
+print(f"Подушка безопасности, чтобы протянуть {months} месяцев без долгов:", round(money_capital))
